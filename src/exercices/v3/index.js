91669ba5df7b8f32278673c4a11b46fb941b8539
WormDragon <dinogopal@yahoo.fr>
2021-12-11 00:04:27 +0100

    please work pls

---
+++ src/exercices/v3/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import DropZone from './dropZone';
+import styli from './dropZone.module.css';
+//var localStorageKey = '_moodboard';
+
+const Grille= () => {
+const [cards, setCard] = React.useState([]);
+const [drop, setDrop] = React.useState(false);
+  const addCard =(img)=> {
+    setCard([...cards, img])
+    setDrop(true)
+  }
+
+  
+
+  console.log(cards)
+
+  return (
+    <div>
+    <div className={styli.frameholder}>
+      {!drop && (
+        <h1>Drag and Drop Here</h1>
+      )}
+          <DropZone addCard={addCard} />  
+          <div className={styli.framer}>  
+      {cards.map((card)=> (
+        <div className={styli.frame}> 
+          <img src={card} width="300px" alt=""/>
+        </div>
+      ))}
+  
+    </div>
+    </div>
+    </div>
+
+  )
+}
+
+export default Grille;
+ 
---
+++ src/exercices/v4/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import DropZone from './dropZone';
+import style from './dropZone.module.css';
+
+
+
+const Grille= () => {
+  const [cards, setCard] = React.useState([]);
+  const [drop, setDrop] = React.useState(false);
+  const [first, setFirst] = React.useState(true);
+  //localStorage.clear()
+  // console.log("drop", drop)
+
+  if (localStorage.getItem('Moodboard') && first===true) { 
+    let cache = JSON.parse(localStorage.getItem('Moodboard'))
+    // console.log("cards",cards)
+    // console.log("cache",cache)
+    // console.log("first", first)
+    // console.log("drop", drop)
+    setCard(cache)
+    setFirst(false)
+    setDrop(true)
+  }
+
+  const addCard =(img)=> {
+    setCard([...cards, img])
+    setFirst(false)
+    setDrop(true)
+    console.log("drop", drop)
+  }
+  React.useEffect(() => {    
+    localStorage.setItem( 'Moodboard' , JSON.stringify(cards) )
+    console.log("cards",cards)
+    let cache = cards
+    console.log("cache",cache)
+    console.log("drop", drop)
+  });
+  return (
+    <div>
+      <div className={style.frameholder}>
+        {!drop && (
+          <h1>Drag and Drop Here</h1>
+        )}
+          <DropZone addCard={addCard} />  
+        <div className={style.framer}> 
+            {cards.map((card)=> (
+          <div className={style.frame}> 
+            <img src={card} width="300px" alt=""/>
+          </div>
+          ))}
+      
+        </div>
+      </div>
+    
+    </div>
+
+  )
+}
+
+export default Grille;
+ 
